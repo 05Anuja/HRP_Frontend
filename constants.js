c14c773afc402f2a4e5f4720adab901a425fcf05
Anuja Pawar <anuja13122004@gmail.com>
2026-09-10 17:26:05 +0530

    bugs resolved

--- constants.js
+++ constants.js
@@ -1,5 +1,5 @@
-export const url = "http://192.168.0.109:5000/api";
-export const uploadLink = "http://192.168.0.109:5000";
+// export const url = "http://192.168.0.109:5000/api";
+// export const uploadLink = "http://192.168.0.109:5000";
 
-// export const url = "http://172.16.40.66:5000/api"; // production
-// export const uploadLink = "http://172.16.40.66:5000"; // production
+export const url = "http://172.16.40.66:5000/api"; // production
+export const uploadLink = "http://172.16.40.66:5000"; // production
